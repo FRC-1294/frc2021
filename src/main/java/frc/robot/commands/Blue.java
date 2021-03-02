ef307d9e422891997b5d759992445d00321e9a94
s-iduan <60229447+s-iduan@users.noreply.github.com>
2021-03-01 18:33:29 -0800

    Red & Blue
    yeet

---
+++ src/main/java/frc/robot/commands/Blue.java
@@ -0,0 +1,52 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DriveAutoSubsystem;
+import frc.robot.commands.MoveByCommand;
+import frc.robot.commands.TurnByCommand;
+
+public class Blue extends SequentialCommandGroup {
+  boolean isFinished = false;
+
+  /** Creates a new Blue. */
+  public Blue(DriveAutoSubsystem driveAuto) {
+    addCommands(MoveByCommand(12.5 * 12, driveAuto, 0));
+    addCommands(TurnByCommand((Math.atan(7.5/2.5) - 45) * -1, driveAuto, 0));
+    addCommands(MoveByCommand(Math.sqrt(62.5) * 12, driveAuto, 0));
+    addCommands(TurnByCommand((180 - Math.atan(2.5/7.5)) * -1, driveAuto, 0));
+    addCommands(MoveByCommand(Math.sqrt(31.25) * 12, driveAuto, 0));
+    addCommands(TurnByCommand(Math.atan(2.5 / 5) * -1, driveAuto, 0));
+    addCommands(MoveByCommand(5 * 12, driveAuto, 0));
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    isFinished = false;
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    isFinished = true;
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    m_robotDrive.setFrontLeftSpeed(0);
+    m_robotDrive.setFrontRightSpeed(0);
+    m_robotDrive.setRearLeftSpeed(0);
+    m_robotDrive.setRearRightSpeed(0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return isFinished;
+  }
+}
---
+++ src/main/java/frc/robot/commands/Red.java
@@ -0,0 +1,61 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DriveAutoSubsystem;
+import frc.robot.commands.MoveByCommand;
+import frc.robot.commands.TurnByCommand;
+
+//ANGLE UNITS = DEGREES
+//LENGTH UNITS = INCHES
+
+public class Red extends SequentialCommandGroup {
+  DriveAutoSubsystem m_driveAuto;
+
+  public Red(DriveAutoSubsystem driveAuto){
+    this.m_driveAuto = driveAuto;
+
+    // amount (degrees or inches), driveAuto, pidType (0)
+    addCommands(TurnByCommand(45, driveAuto, 0));
+    addCommands(MoveByCommand(5 * Math.sqrt(2) * 12, driveAuto, 0));
+    addCommands(TurnByCommand((Math.atan(5/2.5) - 45) * -1, driveAuto, 0));
+    addCommands(MoveByCommand(Math.sqrt(31.25) * 12, driveAuto, 0));
+    addCommands(TurnByCommand((Math.atan(2.5/7.5) + Math.atan(7.5/2.5)) * -1, driveAuto, 0));
+    addCommands(MoveByCommand(Math.sqrt(62.5) * 12, driveAuto, 0));
+    addCommands(TurnByCommand(90 + Math.atan(2.5/7.5), driveAuto, 0));
+    addCommands(MoveByCommand(12.5 * 12, driveAuto, 0));
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    isFinished = false;
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    isFinished = true;
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    m_driveAuto.setFrontLeftSpeed(0);
+    m_driveAuto.setFrontRightSpeed(0);
+    m_driveAuto.setRearLeftSpeed(0);
+    m_driveAuto.setRearRightSpeed(0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return isFinished;
+  }
+}
