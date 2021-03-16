dd27940cee8f4f809265cf4dcc245e033c5e8a2e
s-iduan <60229447+s-iduan@users.noreply.github.com>
2021-03-15 17:54:56 -0700

    Did Red.Java

---
+++ src/main/java/frc/robot/commands/Red.java
@@ -0,0 +1,45 @@
+public class Red extends SequentialCommandGroup {
+  DriveAutoSubsystem m_driveAuto;
+
+  public Red(DriveAutoSubsystem driveAuto){
+    this.m_driveAuto = driveAuto;
+
+    // amount (degrees or inches), driveAuto, pidType (0)
+    addCommands(new MoveByCommand(5 * 12, driveAuto, 0));
+    addCommands(new TurnByCommand(45, driveAuto, 0));
+    addCommands(new MoveByCommand(Math.sqrt(2) * 5 * 12, driveAuto, 0));
+    addCommands(new TurnByCommand(45 * -1, driveAuto, 0));
+    addCommands(new MoveByCommand(Math.sqrt(2) * 5 * 12, driveAuto, 0));
+    addCommands(new TurnByCommand(45 * -1, driveAuto, 0));
+    addCommands(new MoveByCommand(3.5 * 12, driveAuto, 0));
+    addCommands(new TurnByCommand(90, driveAuto, 0));
+    addCommands(new MoveByCommand(10 * 12, driveAuto, 0));
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    isFinished = false;
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    isFinished = true;
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    m_driveAuto.setFrontLeftSpeed(0);
+    m_driveAuto.setFrontRightSpeed(0);
+    m_driveAuto.setRearLeftSpeed(0);
+    m_driveAuto.setRearRightSpeed(0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return isFinished;
+  }
+}
