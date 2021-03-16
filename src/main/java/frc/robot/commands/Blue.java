bdbbb0218dae84254595a60f1aeba0cd338cda4c
s-iduan <60229447+s-iduan@users.noreply.github.com>
2021-03-15 18:33:23 -0700

    Update Blue.java

--- src/main/java/frc/robot/commands/Blue.java
+++ src/main/java/frc/robot/commands/Blue.java
@@ -1,26 +1,30 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DriveAutoSubsystem;
 import frc.robot.commands.MoveByCommand;
 import frc.robot.commands.TurnByCommand;
 
 public class Blue extends SequentialCommandGroup {
-  boolean isFinished = false;
+  DriveAutoSubsystem m_driveAuto;
 
-  /** Creates a new Blue. */
-  public Blue(DriveAutoSubsystem driveAuto) {
-    addCommands(new MoveByCommand(12.5 * 12, driveAuto, 0));
-    addCommands(new TurnByCommand((Math.atan(7.5/2.5) - 45) * -1, driveAuto, 0));
-    addCommands(new MoveByCommand(Math.sqrt(62.5) * 12, driveAuto, 0));
-    addCommands(new TurnByCommand((180 - Math.atan(2.5/7.5)) * -1, driveAuto, 0));
-    addCommands(new MoveByCommand(Math.sqrt(31.25) * 12, driveAuto, 0));
-    addCommands(new TurnByCommand(Math.atan(2.5 / 5) * -1, driveAuto, 0));
-    addCommands(new MoveByCommand(5 * 12, driveAuto, 0));
+  public Blue(DriveAutoSubsystem driveAuto){
+    this.m_driveAuto = driveAuto;
+
+    // amount (degrees or inches), driveAuto, pidType (0)
+    addCommands(new MoveByCommand(15 * 12, driveAuto, 0));
+    addCommands(new TurnByCommand(90 * -1, driveAuto, 0));
+    addCommands(new MoveByCommand(2.5 * 12, driveAuto, 0));
+    addCommands(new TurnByCommand(45, driveAuto, 0));
+    addCommands(new MoveByCommand(Math.sqrt(2) * 5 * 12, driveAuto, 0));
+    addCommands(new TurnByCommand(45 * -1, driveAuto, 0));
+    addCommands(new MoveByCommand(Math.sqrt(2) * 5 * 12, driveAuto, 0));
+    addCommands(new TurnByCommand(45 * -1, driveAuto, 0));
+    addCommands(new MoveByCommand(2 * 12, driveAuto, 0));
+
+    SmartDashboard.putData(new Blue(driveAuto));
   }
 
   // Called when the command is initially scheduled.
@@ -38,10 +42,10 @@ public class Blue extends SequentialCommandGroup {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    m_robotDrive.setFrontLeftSpeed(0);
-    m_robotDrive.setFrontRightSpeed(0);
-    m_robotDrive.setRearLeftSpeed(0);
-    m_robotDrive.setRearRightSpeed(0);
+    m_driveAuto.setFrontLeftSpeed(0);
+    m_driveAuto.setFrontRightSpeed(0);
+    m_driveAuto.setRearLeftSpeed(0);
+    m_driveAuto.setRearRightSpeed(0);
   }
 
   // Returns true when the command should end.
