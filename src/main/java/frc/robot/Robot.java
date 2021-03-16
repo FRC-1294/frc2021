a11017cbd3645d0b0530ed7c7acbdff167b8acb8
Sam Tischaefer <samtisch@live.com>
2021-03-15 18:12:58 -0700

    Added button to shuffleboard

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,8 +8,11 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.commands.SalomPath;
+import frc.robot.subsystems.DriveAutoSubsystem;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -21,6 +24,7 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
+  public DriveAutoSubsystem driveAuto;
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -31,6 +35,8 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+    driveAuto = new DriveAutoSubsystem();
+    SmartDashboard.putData(new SalomPath(driveAuto));
   }
 
   /**
